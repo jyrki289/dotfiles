c15523c4f38cc3248487db2213b231d01e301864
David Alexander <dalexander@ucla.edu>
2012-07-29 16:40:17 -0700

    meh

--- bashrc.sh
+++ bashrc.sh
@@ -1,7 +1,4 @@
 
-source ~/.autojump/autojump.bash
-source ~/.dotfiles/git-completion-1.7.2.2.bash
-
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 alias clean="rm -f *~ *# *.orig"
@@ -10,5 +7,13 @@ alias l='ls'
 alias ll='ls -al' 
 alias reinit="source ~/.bash_profile"
 
-export PS1='\[\e[1m\]\h:\W$(__git_ps1 " (%s)") \$ \[\e[0m\]'
+export CONSENSUSCORE_BUILD_ROOT=/tmp/ConsensusCore
+export CCACHE=ccache
 
+export PBLS_JOB_ROOT=
+export PBLS_REFERENCE_ROOT=/Users/dalexander/Data/References
+
+source ~/.autojump/autojump.bash
+source ~/.dotfiles/git-completion-1.7.2.2.bash
+
+export PS1='\[\e[1m\]\h:\W$(__git_ps1 " (%s)") \$ \[\e[0m\]'
