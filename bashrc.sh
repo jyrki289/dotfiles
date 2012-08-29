cc8d237653990fa0e4686858126a4cac60894e34
David Alexander <dalexander@ucla.edu>
2012-08-29 15:22:47 -0700

    Setup PBLS local repos

--- bashrc.sh
+++ bashrc.sh
@@ -1,3 +1,7 @@
+[[ -f ~/.autojump/etc/profile.d/autojump.bash ]] && source ~/.autojump/etc/profile.d/autojump.bash
+source ~/.dotfiles/git-completion-1.7.2.2.bash
+
+export PS1='\[\e[1m\]\h:\W$(__git_ps1 " (%s)") \$ \[\e[0m\]'
 
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 
@@ -7,13 +11,6 @@ alias l='ls'
 alias ll='ls -al' 
 alias reinit="source ~/.bash_profile"
 
-export CONSENSUSCORE_BUILD_ROOT=/tmp/ConsensusCore
 export CCACHE=ccache
-
-export PBLS_JOB_ROOT=
-export PBLS_REFERENCE_ROOT=/Users/dalexander/Data/References
-
-source ~/.autojump/autojump.bash
-source ~/.dotfiles/git-completion-1.7.2.2.bash
-
-export PS1='\[\e[1m\]\h:\W$(__git_ps1 " (%s)") \$ \[\e[0m\]'
+export PBLS_JOB_ROOT=/Users/dalexander/Data/Jobs/
+export PBLS_REFERENCE_ROOT=/Users/dalexander/Data/References/
