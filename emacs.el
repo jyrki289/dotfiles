eea9dccb1dc764cab09e2f7a077d19675995021d
David Alexander <dalexander@pacificbiosciences.com>
2015-01-06 07:43:01 -0800

    emacs haskell update

--- emacs.el
+++ emacs.el
@@ -134,9 +134,25 @@ try-expand-all-abbrevs try-expand-list))
 ;;
 ;; Haskell
 ;;
-(load "haskell-site-file")
+(require 'haskell-mode-autoloads)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
+;;(define-key haskell-mode-map (kbd "C-x C-s") 'haskell-mode-save-buffer)
+
+;; (eval-after-load "haskell-mode"
+;;   '(progn
+;;     (define-key haskell-mode-map (kbd "C-x C-d") nil)
+;;     (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+;;     (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
+;;     (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
+;;     (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+;;     (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+;;     (define-key haskell-mode-map (kbd "C-c M-.") nil)
+;;     (define-key haskell-mode-map (kbd "C-c C-d") nil)))
+
+
 
 ;;
 ;; Org-mode
